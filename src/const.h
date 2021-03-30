f2a6fc261997ecedb49d6d89c79973cd53106f6a
stop-pattern <railwaysandstationfan+Git@gmail.com>
2021-03-31 05:32:20 +0900

    [add] const
    シフトレジスタ周りで使ってるピンたちの設定

--- src/const.h
+++ src/const.h
@@ -2,7 +2,7 @@
 #define CONST_H
 
 
-// pin num define
+// pin number define
 #ifdef SAHA
 // for rev.2(additionally modificated)
 #define PIN_PWM0 19
@@ -10,6 +10,7 @@
 #endif //SAHA
 
 
+// default pin number define
 #ifndef PIN_PWM0
 #define PIN_PWM0 0
 #endif //PIN_PWM0
@@ -19,9 +20,14 @@
 #endif //PIN_PWM1
 
 
-// pin num settings
+// pin number settings
 constexpr uint8_t PWM0 = PIN_PWM0;
 constexpr uint8_t PWM1 = PIN_PWM1;
+constexpr uint8_t SCL = 32; // shift register clear
+constexpr uint8_t SER = 12; // serial data
+constexpr uint8_t SCK = 33; // shift register clock
+constexpr uint8_t RCK = 27; // storage register clock
+constexpr uint8_t G = 14;   // output enable
 
 
 #undef PIN_PWM0
