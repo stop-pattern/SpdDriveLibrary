3a06ddd8044cfb0bbb27aca4146fffe2d3f9326a
stop-pattern <railwaysandstationfan+Git@gmail.com>
2021-03-31 04:57:08 +0900

    [add] デフォルトの動作
    未定義だったので修正した

--- src/const.h
+++ src/const.h
@@ -1,15 +1,27 @@
 #ifndef CONST_H
 #define CONST_H
 
+
 // pin num define
 #ifdef SAHA
 #define PIN_PWM0 19
 #define PIN_PWM1 18
 #endif //SAHA
 
+
+#ifndef PIN_PWM0
+#define PIN_PWM0 0
+#endif //PIN_PWM0
+
+#ifndef PIN_PWM1
+#define PIN_PWM1 0
+#endif //PIN_PWM1
+
+
 // pin num settings
 constexpr uint8_t PWM0 = PIN_PWM0;
 constexpr uint8_t PWM1 = PIN_PWM1;
+
 
 #undef PIN_PWM0
 #undef PIN_PWM1
